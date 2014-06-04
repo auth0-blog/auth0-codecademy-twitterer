421fd1e82ebe2ec50c0fe8fe9e4867ca5cba3aa9
Martin Gontovnikas <martin@gon.to>
2014-06-04 10:29:40 +0200

    Changed bitcoin text

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/api/finished', function(req, res) {
 
     var text;
     if (req.body.type === 'quiz') {
-      text = "@" + handle + " won FREE Bitcoins at Auth0's booth. #ndcoslo";
+      text = "@" + handle + " solved the Auth0 challenge and won Bitcoins at Auth0's booth. #ndcoslo";
     } else {
       var minutes = 10;
       var seconds = 0;
